128ca488b548bd541a49931279302dd4539a32f2
siamtx <siamtx@yahoo.com>
2024-05-08 20:33:54 -0400

    added js, base.css, design principles

---
+++ lesson02/scripts/getdates.js
@@ -0,0 +1,11 @@
+// Get the current date
+const currentDate = new Date();
+// Extract the current year and weekday
+const currentYear = currentDate.getFullYear();
+const currentWeekday = currentDate.toLocaleString('en-US', { weekday: 'long' });
+// Update the copyright year dynamically
+document.getElementById('currentYear').textContent = `${currentWeekday}, ${currentYear}`;
+//const lastModified  = mod;
+// Update the second paragraph in the footer with the last modified date
+document.getElementById('mod').textContent = `Last Modified: ${currentDate}`;
+//const date = new Date(file.mod);
