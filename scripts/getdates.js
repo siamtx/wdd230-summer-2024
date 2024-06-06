e737e7907d7a49e81d794f49207ae03b2d7938c1
siamtx <siamtx@yahoo.com>
2024-06-05 21:21:53 -0400

    helpo the vistes

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -12,21 +12,10 @@ document.getElementById('currentYear').textContent = `${currentYear}`;
 document.getElementById('mod').textContent = `Last Modified: ${currentDate}`;
 
 
-const forecast = $.ajax
+/*const forecast = $.ajax
 document.getElementById(weather).textContent = `${forecast}`;
 
 var city = 'boston'
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/weather?city=' + city,
-    headers: { 'X-Api-Key': 'Qvf/SRNuDGC8Rr8xxKcXdg==PO4qzaKyFaZV4aI7'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
+*/
 
 /*KEY::::: Qvf/SRNuDGC8Rr8xxKcXdg==PO4qzaKyFaZV4aI7*/
